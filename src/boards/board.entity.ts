1e2eb6798526f0bc5d4e086c21706f927a8e30c1
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-11-16 19:18:25 +0900

    유저와 게시물의 관계 형성 해주기

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,4 +1,11 @@
-import { BaseEntity, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import {
+  BaseEntity,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+} from 'typeorm';
 import { BoardStatus } from './boad-status.enum';
 
 @Entity()
@@ -14,4 +21,7 @@ export class Board extends BaseEntity {
 
   @Column()
   status: BoardStatus;
+
+  @ManyToOne((type) => User, (user) => user.boards, { eager: false })
+  user: User;
 }
